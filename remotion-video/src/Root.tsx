f4eb8b688f6024eeb71f243a763d7120ce9c9899
c9j5nzy665-cmd <franiiito77@gmail.com>
2026-08-28 21:48:28 +0200

    Register Victor Lustig V2 composition

--- remotion-video/src/Root.tsx
+++ remotion-video/src/Root.tsx
@@ -1,10 +1,12 @@
 import "./index.css";
 import {PetRockComposition} from "./Composition";
 import {VictorLustigComposition} from "./VictorLustig";
+import {VictorLustigV2Composition} from "./VictorLustigV2";
 
 export const RemotionRoot: React.FC = () => (
   <>
     <PetRockComposition />
     <VictorLustigComposition />
+    <VictorLustigV2Composition />
   </>
 );
